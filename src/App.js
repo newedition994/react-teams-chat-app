2fba53ccd95cf2eb52add248cd2ee4800d151aa8
Breyanna Vass <breyannavass@gmail.com>
2021-03-04 17:10:15 -0500

    adding in the login form to the main app and importing names

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 
 const App = () => {
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine 
         height = "100vh"
         projectID = {process.env.REACT_APP_PJ_ID_KEY}
-        userName = "reactmastery"
-        userSecret = {process.env.REACT_APP_USER_SECRET}
+        userName = {localStorage.getItem('username')}
+        userSecret = {localStorage.getItem('password')}
 
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
 
     />
   )
